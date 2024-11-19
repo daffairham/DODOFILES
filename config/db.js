367f115206f53251a41d9fb6844d4d941525c02e
daffairham <dafirham@gmail.com>
2024-11-19 23:10:13 +0700

    benerin gitignore

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
-const pg = require('pg')
+const pg = require("pg");
 
 const pool = new pg.Pool({
-    host: 'localhost',
-    user: 'postgres',
-    port: 5432,
-    password: 'postgre123',
-    database: 'dodofiles_db',
-  });
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  port: 5432,
+  password: process.env.DB_PASSWORD,
+  database: "dodofiles_db",
+});
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -1,34 +1,33 @@
+require("dotenv").config();
+
 const express = require("express");
-const bodyParser = require('body-parser');
-const upload = require("express-fileupload")
+const bodyParser = require("body-parser");
+const upload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
 
-const loginController = require("./controller/loginController")
-const registerController = require("./controller/registerController")
-const homeController = require("./controller/homeController")
-const fileController = require("./controller/fileController")
-const sharingController = require("./controller/sharingController")
+const loginController = require("./controller/loginController");
+const registerController = require("./controller/registerController");
+const homeController = require("./controller/homeController");
+const fileController = require("./controller/fileController");
+const sharingController = require("./controller/sharingController");
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT;
 
-
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(upload())
+app.use(upload());
 
-app.use('/', loginController);
-app.use('/', registerController);
-app.use('/', homeController);
-app.use('/', fileController);
-app.use('/', sharingController);
+app.use("/", loginController);
+app.use("/", registerController);
+app.use("/", homeController);
+app.use("/", fileController);
+app.use("/", sharingController);
 
+app.set("view engine", "ejs");
 
-app.set('view engine', 'ejs');
-
-app.listen(PORT, ()=>{
-  console.log(`Listening on PORT ${PORT}`);
-})
-
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
